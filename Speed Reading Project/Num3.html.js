7b2ce7fcfc9f6c51a1f4ac7ec543e34ec75d7c8a
William Writer <williamwriter@Williams-MacBook-Pro-2.local>
2022-05-11 15:49:23 -0600

    This is a push that contains the information me solving my two previous problems that I had last semester

---
+++ Speed Reading Project/Num3.html.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="Speed%20Reader.html"/>
+<script type="text/javascript" src="Num3.html.js"/>
+aListWords.forEach(function (item, index, array) { //turns each word of the arraylist into its own spot in
+    //in the array
+    console.log(item, index); // prints out all of the array parts to the log
+})
+{html}
